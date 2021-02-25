bce9447234546f7523134ac6ffef13ff8d8dc941
catop <601179193@qq.com>
2021-02-25 22:28:31 +0800

    debug

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Descripttion:
 version: 
 Author: Catop
 Date: 2021-02-24 22:21:29
-LastEditTime: 2021-02-25 21:32:42
+LastEditTime: 2021-02-25 22:26:55
 '''
 from flask import Flask,request,jsonify
 import json
@@ -39,8 +39,8 @@ def getEvent():
             print(f"接收加好友请求@{user_id}:{comment[:20]}")
             time.sleep(random.randint(5,10))
             goapi.add_request(flag)
+            time.sleep(random.randint(5,10))
             goapi.sendMsg(user_id,readMsg.user_tips)
-
     else:
         #防止go-cq上报错误
         pass
